a7b64c18fdfde1241d8e180f3f38b998cd0bdb91
林鈺錦 <abc1236762@outlook.com>
2020-06-09 02:01:07 +0800

    init

--- src/plugins/tensorflow.js
+++
@@ -1,9 +0,0 @@
-import * as tf from '@tensorflow/tfjs';
-
-if (typeof global !== 'undefined' && global.toString() === '[object global]') {
-  require('@tensorflow/tfjs-node');
-} else {
-  require('@tensorflow/tfjs-backend-webgl');
-}
-
-export default tf;
